dee8a063b08dedd84899fdc286c7ea0e6e04f0e1
Rishabh <rishabhgargts@gmail.com>
2022-12-30 00:09:13 +0530

    Home: Log-out

--- App.js
+++ App.js
@@ -1,22 +1,56 @@
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View , Dimensions,Animated} from 'react-native';
-import CustomComponent from './src/screens/CustomComponent';
-import VideoPlayer from './src/plugins/videoPlayer';
-import QrCodePage from './src/screens/QrCodePage';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Dimensions, Animated } from "react-native";
+import CustomComponent from "./src/screens/CustomComponent";
+import VideoPlayer from "./src/plugins/videoPlayer";
+import QrCodePage from "./src/screens/QrCodePage";
+import { NavigationContainer } from "@react-navigation/native";
+import {
+  createNativeStackNavigator,
+  TransitionPresets,
+  CardStyleInterpolators,
+} from "@react-navigation/native-stack";
+import Home from "./src/screens/Home";
 
 export default function App() {
- 
+  const Stack = createNativeStackNavigator();
+  const config = {
+    animation: "spring",
+    config: {
+      stiffness: 1000,
+      damping: 50,
+      mass: 3,
+      overshootClamping: true,
+      restDisplacementThreshold: 0.01,
+      restSpeedThreshold: 0.01,
+    },
+  };
   return (
-    <View style={styles.container}> 
-    
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{ 
+          transitionSpec: { open: config } }}
+        initialRouteName="VideoPlayer"
+      >
+        <Stack.Screen
+          name="VideoPlayer"
+          component={VideoPlayer}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+    //   <View style={styles.container}>
 
-    {/* <QrCodePage /> */}
-  {/*  */}
-    {/* <Text style={{color: '#fff', fontSize: 30}}>Hello World</Text> */}
-    <VideoPlayer />
-  </View>
-  
+    //   {/* <QrCodePage /> */}
+    // {/*  */}
+    //   {/* <Text style={{color: '#fff', fontSize: 30}}>Hello World</Text> */}
+    //   <VideoPlayer />
+    // </View>
   );
 }
 const styles = StyleSheet.create({
@@ -24,9 +58,8 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#F0BD30",
 
-    alignItems: 'center',
-    justifyContent: 'center',
-    position: 'relative',
-
+    alignItems: "center",
+    justifyContent: "center",
+    position: "relative",
   },
 });
---
+++ src/screens/Home.js
@@ -0,0 +1,74 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Button,
+  Pressable,
+} from "react-native";
+import React from "react";
+
+const Home = () => {
+  return (
+    <View style={styles.container}>
+      <Pressable
+        onPressIn={() => {
+          alert("Restart the app to login again");
+        }}
+      >
+        <Text style={styles.signup}>LOG-OUT!</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#F0BD30",
+    alignItems: "center",
+    justifyContent: "center",
+    position: "relative",
+  },
+  background: {
+    width: "100%",
+    height: "100%",
+  },
+  logo: {
+    width: 280,
+    height: 280,
+    marginLeft: "15%",
+    marginTop: "10%",
+  },
+  text: {
+    color: "white",
+    marginTop: "-25%",
+    marginLeft: "20%",
+  },
+  signup: {
+    backgroundColor: "white",
+    color: "#F0BD30",
+    width: "75%",
+    borderRadius: 25,
+    textAlign: "center",
+    fontWeight: "bold",
+    //   marginLeft: '11%',
+    padding: "2%",
+    fontSize: 27,
+    //   marginTop: '70%'
+  },
+  login: {
+    backgroundColor: "#3A59FF",
+    color: "white",
+    width: "75%",
+    borderRadius: 25,
+    textAlign: "center",
+    fontWeight: "bold",
+    marginLeft: "11%",
+    padding: "2%",
+    fontSize: 27,
+    marginTop: "10%",
+  },
+});
